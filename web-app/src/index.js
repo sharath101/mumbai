07ebca322cf7d4487b22d13500487ea35798f984
Sharath Nair <sharathmappu99@gmail.com>
2022-12-12 01:50:09 +0530

    feat: login form

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId = "1050794157998-61nk4mnad5kkv1mg2hjc1rm3a8hpgteh.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
