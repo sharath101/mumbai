42b2a7c642e8ca1e82f3383e3e0f18e69bd519d1
amans-ingh <amansingh160899@gmail.com>
2022-12-22 22:42:58 +0530

    branched

--- webapi/config.sample.py
+++ webapi/config.sample.py
@@ -3,3 +3,7 @@ SQLALCHEMY_DATABASE_URI = "postgresql://<user>:<pass>@<ip>:<port>/<db>"
 SERVER_URL = 'http://hostname:port'
 STEAM_API_KEY = 'steam_api_key'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+PROFILE_PICS = r"profile_pics_directory"
+STEAM_API_LINK1 = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key="
+STEAM_API_LINK2 = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="
+
